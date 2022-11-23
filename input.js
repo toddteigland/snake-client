7b9109d740fe2d9deb61310a0cc4da9c95ba8212
toddteigland <toddteigland@gmail.com>
2022-11-23 15:51:03 -0800

    Created fil to handle all user imputs, and connected it with the connection from client.js

---
+++ input.js
@@ -0,0 +1,34 @@
+let connection;
+
+const handleUserInput = function (key) {
+  console.log(key);
+  if (key === '\u0003') {
+    process.exit();
+  }
+  if (key === 'w') {
+    connection.write(`Move: up`);
+  }
+  if (key === 'a') {
+    connection.write(`Move: left`);
+  }
+  if (key === 's') {
+    connection.write(`Move: down`);
+  }
+  if (key === 'd') {
+    connection.write(`Move: right`);
+  }
+};
+
+const setupInput = function (conn) {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+
+
+module.exports = {setupInput};
