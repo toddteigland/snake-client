7aeb9f6ff30d3f6bc9164574738a331195105f02
toddteigland <toddteigland@gmail.com>
2022-11-23 13:50:55 -0800

    Adding a name to my snake

--- client.js
+++ client.js
@@ -1,4 +1,5 @@
 const net = require("net");
+const name = 'TDT';
 
 const connect = function () {
   const conn = net.createConnection({
@@ -9,6 +10,14 @@ const connect = function () {
   conn.on('data', (data) => {
     console.log(data);
   })
+
+  conn.on('connect',() => {
+  console.log('Connection established');  
+  })
+
+  conn.on("connect", () => {
+    conn.write(`Name: ${name}`);
+  });
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
